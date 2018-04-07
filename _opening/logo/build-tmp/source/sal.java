2a4ba2851f2d6058d559b33bf1866ad583f27aef
Hansimov <591172499@qq.com>
2018-04-07 16:39:12 +0800

    ^ Reorgnize structure of files

---
+++ _opening/logo/build-tmp/source/sal.java
@@ -0,0 +1,35 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class sal extends PApplet {
+  public void setup() {
+
+// background(0);
+noStroke();
+
+fill(255,0,0);
+ellipse(100, 100, 50, 50);
+    noLoop();
+  }
+
+  public void settings() { size(1280, 720); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--stop-color=#cccccc", "sal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ _opening/logo/logo_v1.js
@@ -0,0 +1,146 @@
+pi = Math.PI;
+cos = Math.cos;
+sin = Math.sin;
+floor = Math.floor;
+
+
+
+function setup(){
+    cnvs = createCanvas(480, 270); // used in logo
+    noLoop();
+}
+
+
+function draw() {
+    cx = 240;
+    cy = 120;
+    rmax = 160;
+    drawLogo();
+    // drawSBTable();
+    // drawGrayTable();
+
+}
+
+function drawLogo() {
+    background(20);
+    outerCircle();
+    innerCircle();
+}
+
+function outerCircle() {
+    colorMode(HSB,255);
+
+    stroke(170,10,105);
+    strokeWeight(20);
+    ellipse(cx,cy,rmax-1,rmax-1);
+    stroke(170,10,165);
+    strokeWeight(13);
+    ellipse(cx,cy,rmax-1,rmax-1);
+
+
+    // stroke(170,10,225);
+    // strokeWeight(10);
+    // noFill();
+    // arclen = pi/20;
+    // for (var i=1; i<=3; i++){
+    //     arc(cx,cy,rmax+4,rmax+4,i*pi/3-arclen,i*pi/3+arclen,OPEN);
+    // }
+
+
+    // stroke(170,10,205);
+    // strokeWeight(10);
+    // ellipse(cx,cy,rmax-1,rmax-1);
+
+    // for (var i=25; i>=0; i--){
+    //     if (i>=15){
+    //         stroke(170, 20, 200);
+    //     } else {
+    //         stroke(170, 20, 100);
+    //     }
+    //     ellipse(cx,cy,rmax+i,rmax+i);
+    // }
+    // strokeWeight(10);
+    // stroke(170, 20, 100);
+    // ellipse(cx,cy,rmax+25,rmax+25);
+    // stroke(170, 20, 150);
+    // strokeWeight(5);
+    // ellipse(cx,cy,rmax+15,rmax+15);
+    // strokeWeight(1);
+
+}
+
+function innerCircle () {
+    ratio = rmax/255;
+    noStroke();
+    colorMode(HSB,255);
+    for (var r = 255; r >= 0; r--){
+        changeFill(r);
+        ellipse(cx, cy, r*ratio, r*ratio);
+    }
+
+    function changeFill(r) {
+        // Hue, Saturation, Brightness, alpha
+
+        var k = 400;
+        var theta = (255-r)/255*(pi/2);
+        var sat = floor(k*cos(theta));
+        var bri = floor(k*sin(theta));
+        if (r>=130){
+            fill(170, sat, bri-70);
+        } else if (r>=30) {
+            fill(170, sat, bri);
+        } else {
+            fill(170, sat-6, bri);
+        }
+    }
+}
+
+
+function invR(r,max=255){
+    var r_inv = max - r;
+    return r_inv;
+}
+
+function drawSBTable(){
+    createCanvas(350,380); // used in sbtable
+    var px = 60;
+    var py = 40;
+    colorMode(HSB,255);
+    background(255);
+
+    for (var s=0; s<=255; s++) {
+        for (var b=0; b<=255; b++){
+            stroke(170,s,255-b);
+            point(px+s,py+b);
+        }
+    }
+    addText();
+
+    function addText(){
+        // textFont('Monospaced');
+        textAlign(RIGHT,CENTER);
+        text('255', px-10,      py);
+        text('0',   px-10,      py+256);
+        text('亮',px-35,py+120);
+        text('度',px-35,py+140);
+        textAlign(CENTER);
+        text('0',   px,     py+270);
+        text('255', px+256, py+270);
+        text('饱 和 度',px+128,py+295);
+    }
+}
+
+function drawGrayTable(){
+    createCanvas(350,380); // used in graytable
+    var px = 60;
+    var py = 40;
+    colorMode(RGB,255);
+    background(255);
+
+    for (var row=0; row<=255; row++){
+        for (var col=0; col<=50; col++){
+            stroke(row);
+            point(px+row,py+col);
+        }
+    }
+}
