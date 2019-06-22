2cee901db9fcef368ffcc08902aafdf49524e857
Hansimov <591172499@qq.com>
2019-06-23 00:57:41 +0800

    + Add word cloud test

--- bili-ten-years/word_cloud.py
+++ bili-ten-years/word_cloud.py
@@ -2,12 +2,14 @@ from tikzpy import *
 import os
 import csv
 import subprocess
-
+from random import random, randint
 
 glyphsfolder = "glyphs"
 glyphsfile = glyphsfolder + ".pdf"
 if not os.path.exists(glyphsfolder): 
     os.mkdir(glyphsfolder)
+
+wordsfile = "words.pdf"
 
 abspath = os.getcwd()
 # cmd_pdf2png = 'gswin64c -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pngalpha -r{0} -sOutputFile="{1}\\glyphs\\glyphs_%05d.png" "{1}\\glyphs.pdf"'.format(72,abspath)
@@ -19,26 +21,22 @@ def glyphs2xy(glyphs,font_face="Arial Unicode MS",font_size=100):
     nd1 = node(xy=[pw/2,ph/2],text=glyphs,font_face=font_face,font_size=font_size,text_rgba=[0,0,0,1])
     name,ext = os.path.splitext(glyphsfile)
 
-initTikzpy(glyphsfile,width=0,height=0)
-glyphs2xy("HELLO你好","微软雅黑",30)
-glyphs2xy("你好世界","微软雅黑",30)
-glyphs2xy("英雄联盟","微软雅黑",30)
-glyphs2xy("搞笑","微软雅黑",30)
-glyphs2xy("鬼畜","微软雅黑",30)
+# os.system(cmd_pdf2png)
+
+word_freq_data = "./word-freq-data/"
+
+fs = os.listdir(word_freq_data)
+
+csv_reader = csv.reader(open(word_freq_data+fs[57],mode="r",encoding="utf8"),delimiter=",")
+lines = list(csv_reader)
+
+
+initTikzpy(wordsfile,width=1280,height=720)
+for idx, line in enumerate(lines):
+    # if idx == 10:
+    #     break
+    # print(line)
+    x,y = randint(100,1100), randint(100,600)
+    r,g,b = random(),random(),random()
+    node(text_rgba=[r,g,b,1.0],xy=[x,y],font_size=randint(10,40),text=line[0])
 outputImg()
-
-os.system(cmd_pdf2png)
-
-# word_freq_data = "./word-freq-data/"
-
-# fs = os.listdir(word_freq_data)
-
-# csv_reader = csv.reader(open(word_freq_data+fs[57],mode="r",encoding="utf8"),delimiter=",")
-# lines = list(csv_reader)
-
-# for idx,line in enumerate(lines):
-#     if idx == 10:
-#         break
-#     print(line)
-# print(rows)
-# print(len(rows))
