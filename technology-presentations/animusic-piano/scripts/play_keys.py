d2173144ffed89ed68d3da88f38ff081235fbdf6
Hansimov <591172499@qq.com>
2020-02-28 00:08:38 +0800

    + Optimize num conversions

--- technology-presentations/animusic-piano/scripts/play_keys.py
+++ technology-presentations/animusic-piano/scripts/play_keys.py
@@ -1,6 +1,6 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-os.environ['SDL_VidxEO_CENTERED'] = '1'
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 import pygame as pg
 import sys
 import random
@@ -69,79 +69,103 @@ clock = pg.time.Clock()
 
 fps_font = pg.font.SysFont("comicsansms", 30)
 num_font = pg.font.SysFont("arial bold", 20)
-pit_font = pg.font.SysFont("arial bold", 20)
+pit_font = pg.font.SysFont("arial bold", 17)
+
+# count : w: 7*7+3=52 | b: 1+5*7=36
+
+def num_to_wb(num):
+    return "w" if num<=52 else "b"
+
+# pit_list = ["C","Cs","D","Ds","E","F","Fs","G","Gs","A","As","B"]
+# b_pit_list = ["Bb","Db","Eb","Gb","Ab"]
+b_pit_list = ["As","Cs","Ds","Fs","Gs"]
+def num_to_pit(num):
+    # num:  1  2    3  4  5  6  7  8  9   10     52
+    # pit: A0 B0 - C1 D1 E1 F1 G1 A1 B1 - C2 ... C8
+    if num <= 52:
+        pit = chr(ord("A")+(num-1)%7) + str((num+4)//7)
+    # num:  53    54  55  56  57  58    59      88
+    # pit: Bb0 - Db1 Eb1 Gb1 Ab1 Bb1 - Db2 ... Bb7
+    #      As0 - Cs1 Ds1 Fs1 Gs1 As1 - Cs2 ... As7
+    else:
+        pit = b_pit_list[(num-53)%5] + str((num-53+4)//5)
+    return pit
+
+w_idx_list = [1, 3, 4, 6, 8, 9, 11]
+b_idx_list = [2, 5, 7, 10, 0]
+def num_to_idx(num):
+    # num: 1 2 3 4 5 6  7 -  8  9 10 11 12 13 14 - 15 16 ...  47 48 49 - 50 51 52
+    # idx: 1 3 4 6 8 9 11 - 13 15 16 18 20 21 23 - 25 27 ...  80 81 83 - 85 87 88
+    #      1  2  1  2   1 -  1   2    1   2    1 -  1   2       2    1 -  1   2 
+    if num <= 52:
+        idx = w_idx_list[(num-1)%7] + ((num-1)//7)*12
+    # num: 53 54 55 56 57 - 58 59 60 61 62 - 63 64 65 ... 83 84 85 86 87 - 88
+    # idx:  2  5  7 10 12 - 14 17 19 22 24 - 26 29 31 ... 74 77 79 82 84 - 86
+    else:
+        idx = b_idx_list[(num-53)%5] + ((num-52)//5)*12
+    return idx
+
+def idx_to_num(idx):
+    # idx: 1 2  3 4  5 6  7 8 9 10 11 12 - 13 14 15 16 17 18 19 20 21 22 23 24 ... 88
+    # num: 1 53 2 3 54 4 55 5 6 56  7 57 -  8 58  9 10 59 11 60 12 13 61 14 62 ... 52
+    idx_mod_12 = idx % 12
+    idx_div_12 = (idx-1) // 12
+    if idx_mod_12 in w_idx_list:
+        num = w_idx_list.index(idx_mod_12) + 1 + idx_div_12 * 7
+    else:
+        num = b_idx_list.index(idx_mod_12) + 53 + idx_div_12 * 5
+    return num
+
+x_off, y_off = 20, 200
+key_w, key_h = 18, 60
+gap_w = 4
+wb_gap = 100
+def num_to_xy(num):
+    if num <= 52:
+        x_tmp = x_off + num * (key_w + gap_w)
+        y_tmp = y_off
+    else:
+        x_tmp = x_off + (num-52) * (key_w + gap_w)
+        y_tmp = y_off - wb_gap
+    return (x_tmp,y_tmp)
+
+def num_to_color(num):
+    if num <= 52:
+        color = (255,255,255)
+    else:
+        color = (90,90,90)
+    return color
 
 class Key():
-    # count : w: 7*7+3=52 | b: 1+5*7=36
-    # b_mod_12_list = [0,2,5,7,10]
-    # b_pit_list= ["Bb","Db","Eb","Gb","Ab"]
-    b_pit_list= ["As","Cs","Ds","Fs","Gs"]
-    # pit_list = ["C","Cs","D","Ds","E","F","Fs","G","Gs","A","As","B"]
-    w_idx_list = [1, 3, 4, 6, 8, 9, 11]
-    b_idx_list = [2, 5, 7, 10, 12]
-    key_w, key_h = 18, 60
-    x_tmp, y_tmp = 20, 140
-
     def __init__(self):
-        self.x, self.y, self.w, self.h = (100,100,20,20)
-
-    def num_to_wb(self):
-        # white: idx % 12 = 1,3,4,6,8,9,11
-        # black: idx % 12 = 0,2,5,7,10
-        if self.num <= 52:
-            self.wb = "w"
-        else:
-            self.wb = "b"
-
-    def num_to_pit(self):
-        # num:  1  2    3  4  5  6  7  8  9   10     52
-        # pit: A0 B0 - C1 D1 E1 F1 G1 A1 B1 - C2 ... C8
-        if self.num <= 52:
-            self.pit = chr(ord("A")+(self.num-1)%7) + str((self.num+4)//7)
-        # num:  53    54  55  56  57  58    59      88
-        # pit: Bb0 - Db1 Eb1 Gb1 Ab1 Bb1 - Db2 ... Bb7
-        #      As0 - Cs1 Ds1 Fs1 Gs1 As1 - Cs2 ... As7
-        else:
-            self.pit = b_pit_list[(self.num-53)%5] + str((self.num-53+4)//5)
-
-    def pit_to_idx(self):
-        # pit: A0 As0 B0 - C1 Cs1 D1 Ds1 E1 F1 Fs1 G1 Gs1 A1 As1 B1 - C2 Cs2 ... - C8
-        # idx:  1   2  3    4   5  6   7  8  9  10 11  12 13  14 15   16  17 ...   88
         pass
-    def num_to_idx(self):
-        # num: 1 2 3 4 5 6  7 -  8  9 10 11 12 13 14 - 15 16 ...  47 48 49 - 50 51 52
-        # idx: 1 3 4 6 8 9 11 - 13 15 16 18 20 21 23 - 25 27 ...  80 81 83 - 85 87 88
-        #      1  2  1  2   1 -  1   2    1   2    1 -  1   2       2    1 -  1   2 
-        if self.num <= 52:
-            self.idx = w_idx_list[(self.num-1)%7] + ((self.num-1)//7)*12
-        # num: 53 54 55 56 57 - 58 59 60 61 62 - 63 64 65 ... 83 84 85 86 87 - 88
-        # idx:  2  5  7 10 12 - 14 17 19 22 24 - 26 29 31 ... 74 77 79 82 84 - 86
-        else:
-            self.idx = b_idx_list[(self.num-53)%5] + ((self.num-53)//5)*12
 
-    def setup_wb_pit_pos_num(self):
-        if (self.wb == "w"):
-            self.color = (255,255,255)
-        else:
-            self.color = (90,90,90)
+    def num_to_all(self):
+        self.wb = num_to_wb(self.num)
+        self.pit = num_to_pit(self.num)
+        self.idx = num_to_idx(self.num)
+        self.color = num_to_color(self.num)
+        self.x, self.y = num_to_xy(self.num)
+        self.w, self.h = key_w, key_h
 
     def disp(self):
+        self.num_to_all()
         self.rect = pg.Rect(self.x, self.y, self.w, self.h)
-        self.color = (125,255,random.randint(0,255))
+        # self.color = (125,255,random.randint(0,255))
         pg.draw.rect(screen,self.color,self.rect)
 
-        num_text = str(self.idx)
-        num_text_w, num_text_h = idx_font.size(num_text)
+        num_text = str(self.num)
+        num_text_w, num_text_h = num_font.size(num_text)
         num_text_x = self.x + (self.w - num_text_w)//2
         num_text_y = self.y + self.h + 4
-        num_text_rd = idx_font.render(num_text, 1, (255,255,0))
+        num_text_rd = num_font.render(num_text, 1, (255,255,0))
         screen.blit(num_text_rd,(num_text_x,num_text_y))
 
         pit_text = self.pit
-        pit_text_w, pit_text_h = pit_font.size(self.pit_text)
+        pit_text_w, pit_text_h = pit_font.size(pit_text)
         pit_text_x = self.x + (self.w - pit_text_w)//2
         pit_text_y = self.y - pit_text_h - 2
-        pit_text_rd = pit_font.render(self.pit_text, 1, (255,255,255))
+        pit_text_rd = pit_font.render(pit_text, 1, (255,255,255))
         screen.blit(pit_text_rd,(pit_text_x,pit_text_y))
 
 key_list = []
