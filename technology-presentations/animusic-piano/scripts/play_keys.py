fb97830f4a03a4e5d8505b62c3db9869867565cb
Hansimov <591172499@qq.com>
2020-02-29 00:28:39 +0800

    + Improve several function structure

--- technology-presentations/animusic-piano/scripts/play_keys.py
+++ technology-presentations/animusic-piano/scripts/play_keys.py
@@ -5,6 +5,7 @@ import pygame as pg
 import sys
 import random
 import collections
+from operator import add, sub
 
 img_path = "./frames"
 if not os.path.exists(img_path):
@@ -20,6 +21,7 @@ SEC_PER_BAR = SEC_PER_BEAT * NUME
 SEC_WHOLE_NOTE = SEC_PER_BEAT * DENO
 SEC_QUART_NOTE = SEC_PER_BEAT * DENO / 4
 
+frm = 0
 WIN_WH = (1200, 720//2)
 
 def sec_to_frm(sec):
@@ -28,7 +30,7 @@ def frm_to_sec(frm):
     sec = frm * FPS
 
 def gen_notes():
-    # return list of [start_sec, dura_sec, note_idx] lists
+    # return list of [start_sec, dura_sec, note_idx]
 
     # note_cnt = 200
     # whole note is 1
@@ -61,18 +63,9 @@ def gen_notes():
     #     print(note_cnt)
     print(collections.Counter(note_cnt_list))
 
-    return(notes_list)
-
-pg.init()
-screen = pg.display.set_mode(WIN_WH, flags=pg.RESIZABLE)
-clock = pg.time.Clock()
-
-fps_font = pg.font.SysFont("comicsansms", 30)
-num_font = pg.font.SysFont("arial bold", 20)
-pit_font = pg.font.SysFont("arial bold", 17)
+    return notes_list
 
 # count : w: 7*7+3=52 | b: 1+5*7=36
-
 def num_to_wb(num):
     return "w" if num<=52 else "b"
 
@@ -116,97 +109,140 @@ def idx_to_num(idx):
         num = b_idx_list.index(idx_mod_12) + 53 + idx_div_12 * 5
     return num
 
+w_color = (255,255,255)
+b_color = (90,90,90)
+g_color = (0,200,0)
+def num_to_color(num):
+    return w_color if num<=52 else b_color
+
+w_key_w, w_key_h = 18, 60
+b_key_w, b_key_h = 14, 40
 x_off, y_off = 20, 200
-key_w, key_h = 18, 60
 gap_w = 4
-wb_gap = 100
-def num_to_xy(num):
+wb_y_off = 80
+bef_cnt_list = [1,3,4,6,7]
+def num_to_whxy(num):
     if num <= 52:
-        x_tmp = x_off + num * (key_w + gap_w)
+        w_tmp, h_tmp = (w_key_w, w_key_h)
+    else:
+        w_tmp, h_tmp = (b_key_w, b_key_h)
+
+    if num <= 52:
+        x_tmp = x_off + num * (w_key_w + gap_w)
         y_tmp = y_off
     else:
-        x_tmp = x_off + (num-52) * (key_w + gap_w)
-        y_tmp = y_off - wb_gap
-    return (x_tmp,y_tmp)
+        # num-53: 0 1 2 3 4 - 5  6  7  8  9 - ... - 35
+        # before: 1 3 4 6 7 - 8 10 11 13 14 - ... - 50
+        x_tmp = x_off + (bef_cnt_list[(num-53)%5] + ((num-53)//5)*7)*(w_key_w+gap_w) \
+              + w_key_w - b_key_w//2 + gap_w//2
+        y_tmp = y_off - wb_y_off
 
-def num_to_color(num):
-    if num <= 52:
-        color = (255,255,255)
-    else:
-        color = (90,90,90)
-    return color
+    return (w_tmp, h_tmp, x_tmp, y_tmp)
+
+pg.init()
+screen = pg.display.set_mode(WIN_WH, flags=pg.RESIZABLE)
+clock = pg.time.Clock()
+
+fps_font = pg.font.SysFont("comicsansms", 30)
+num_font = pg.font.SysFont("arial bold", 20)
+pit_font = pg.font.SysFont("arial bold", 17)
 
 class Key():
-    def __init__(self):
-        pass
+    def __init__(self, num):
+        self.num = num
+        self.num_to_all()
+        self.rect = pg.Rect(self.x, self.y, self.w, self.h)
+        self.is_play = False
+        self.end_frm = -1
 
     def num_to_all(self):
         self.wb = num_to_wb(self.num)
         self.pit = num_to_pit(self.num)
         self.idx = num_to_idx(self.num)
+        self.w, self.h, self.x, self.y = num_to_whxy(self.num)
         self.color = num_to_color(self.num)
-        self.x, self.y = num_to_xy(self.num)
-        self.w, self.h = key_w, key_h
 
-    def disp(self):
-        self.num_to_all()
-        self.rect = pg.Rect(self.x, self.y, self.w, self.h)
-        # self.color = (125,255,random.randint(0,255))
-        pg.draw.rect(screen,self.color,self.rect)
-
+    def disp_text(self):
         num_text = str(self.num)
         num_text_w, num_text_h = num_font.size(num_text)
-        num_text_x = self.x + (self.w - num_text_w)//2
-        num_text_y = self.y + self.h + 4
+        num_text_xy = tuple(map(sub,self.rect.midbottom, (num_text_w//2,-3)))
         num_text_rd = num_font.render(num_text, 1, (255,255,0))
-        screen.blit(num_text_rd,(num_text_x,num_text_y))
+        screen.blit(num_text_rd,num_text_xy)
 
         pit_text = self.pit
         pit_text_w, pit_text_h = pit_font.size(pit_text)
-        pit_text_x = self.x + (self.w - pit_text_w)//2
-        pit_text_y = self.y - pit_text_h - 2
+        pit_text_xy = tuple(map(sub,self.rect.midtop, (pit_text_w//2,pit_text_h+2)))
         pit_text_rd = pit_font.render(pit_text, 1, (255,255,255))
-        screen.blit(pit_text_rd,(pit_text_x,pit_text_y))
+        screen.blit(pit_text_rd, pit_text_xy)
+
+    @property
+    def is_play(self):
+        return self._is_play
+    @is_play.setter
+    def is_play(self,val):
+        self._is_play = val
+        if val == True:
+            self.color = g_color
+        else:
+            self.color = num_to_color(self.num)
+
+        
+    def disp_rect(self):
+        # self.color = (125,255,random.randint(0,255))
+        if self.is_play:
+            self.color = (0,255,0)
+        else:
+            self.color = num_to_color(self.num)
+        pg.draw.rect(screen,self.color,self.rect)
+
+    def disp(self):
+        self.disp_rect()
+        self.disp_text()
 
 key_list = []
-def createKeyList():
+def create_key_list():
     global key_list
     for i in range(88):
-        key_tmp = Key()
-        key_tmp.num = i+1
+        key_tmp = Key(i+1)
         key_list.append(key_tmp)
 
-createKeyList()
-
-def drawKeyList():
+def draw_key_list():
     for key in key_list:
         key.disp()
 
 def quiter():
-    clock.tick_busy_loop(30)
+    clock.tick_busy_loop(FPS)
     for event in pg.event.get():
         if (event.type == pg.QUIT) or \
            (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
             pg.quit()
             sys.exit()
 
-frm = 0
-
-def drawFPS():
+def draw_fps():
     global frm
     frm += 1
     fps_text = fps_font.render(str(frm),1,(0,255,0))
     screen.blit(fps_text,(0,0))
 
+def notes_into_keys():
+    # list of [start_sec, dura_sec, note_idx]
+    # notes_list = gen_notes()
+    # for note in gen_notes:
+    #     start_frm_tmp, dura_frm_tmp = sec_to_frm(note[0]), sec_to_frm(note[1])
+    #     key_list[note[2]].notes_list.append([start_frm_tmp,dura_frm_tmp])
+    pass
+
 def main():
+    create_key_list()
+    
     while True:
         quiter()
         screen.fill((0,0,0))
-        drawFPS()
-        drawKeyList()
+        draw_fps()
+        key_list[3].is_play = True if frm%2==1 else False
+        draw_key_list()
         pg.display.update()
         # pg.image.save(screen,img_path+"/{:0>5d}.jpg".format(frm))
 
 if __name__ == '__main__':
     main()
-    # gen_notes()
