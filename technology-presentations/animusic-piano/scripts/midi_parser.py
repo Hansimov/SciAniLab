42f8d921dd7c1a9988f6ab003f8ac32ac2da6759
Hansimov <591172499@qq.com>
2020-03-29 20:26:18 +0800

    + Add midi insert_note

--- technology-presentations/animusic-piano/scripts/midi_parser.py
+++ technology-presentations/animusic-piano/scripts/midi_parser.py
@@ -8,13 +8,21 @@
 # ** https://m.midifan.com/article_body.php?id=901
 
 # * Standard MIDI file format, updated 
-# ** http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BMA2_
+# ** http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
+
+# * Note names, MIDI numbers and frequencies 
+# ** https://newt.phys.unsw.edu.au/jw/notes.html
+
+from __future__ import print_function, division
 
 with open("abc.mid","rb") as rf:
+# with open("secret1.mid","rb") as rf:
     bytes_L = ["{:02x}".format(b) for b in rf.read()]
 
 hexchr = "0123456789abcdef"
-# ptr = 0
+
+TPQN = -1   # ticks per quarter-note
+USPQN = -1  # us per quarter-note
 
 def get_bytes(start,end=None):
     if end == None:
@@ -35,26 +43,70 @@ def hex2bin(hex_str):
     bit_num = len(hex_str)*4 
     return "{val:0{width}b}".format(val=hex2dec(hex_str),width=bit_num)
 
-def get_delta_time(ptr):
+def parse_delta_time(ptr):
     """ return (int, int) : (pointer offset, num of tick) """
     # * Variable Length Values 
-    # ** http://www.ccarh.org/courses/253/handout/vlv/    hex_str = get_bytes(ptr)
-    bin_str = "{:08b}".format(hex2dec(hex_str))
+    # ** http://www.ccarh.org/courses/253/handout/vlv/
+    # hex_str = 
+    # bin_str = "{:08b}".format(hex2dec(hex_str))
+    byte = get_bytes(ptr)
+    bin_str = hex2bin(byte)
     if bin_str[0]=="0":
-        return bin2dec(bin_str[1:]), ptr+1
-
-    bin_str = bin_str[1:]
-    ptr += 1
-    tmp_str = get_bytes(ptr)
-    while tmp_str[0]!="0":
-        bin_str+=tmp_str[1:]
+        pass
+    else:
+        bin_str = bin_str[1:]
         ptr+=1
-    bin_str+=tmp_str[1:]
+        tmp_str = hex2bin(get_bytes(ptr))
+        while tmp_str[0]!="0":
+            bin_str+=tmp_str[1:]
+            ptr+=1
+        bin_str+=tmp_str[1:]
+    dt = bin2dec(bin_str)
     ptr+=1
-    return ptr, bin2dec(bin_str)
+    print(f"dt: {dt} (ticks)")
+    return ptr, dt
 
-def get_event(ptr):
-    """ return event string """
+def parse_header():
+    global TRK_NUM, TPQN
+    ptr = 0
+    if get_bytes(ptr,ptr+4) == "4d546864":
+        print("=== MThd ===")
+    else:
+        print("x Cannot parse MThd!")
+        return ptr, False
+
+    ptr+=4
+    mthd_chunk_len = hex2dec(get_bytes(ptr,ptr+4))
+    print(f"MThd chunk length: {mthd_chunk_len} (bytes)")
+
+    ptr+=4
+    TRK_FMT = hex2dec(get_bytes(ptr,ptr+2))
+    print(f"Track Format: {TRK_FMT}")
+
+    ptr+=2
+    TRK_NUM = hex2dec(get_bytes(ptr,ptr+2))
+    print(f"Track Number: {TRK_NUM}")
+
+    ptr+=2
+    TPQN = hex2dec(get_bytes(ptr,ptr+2))
+    print(f"Ticks Per Quarter-Note: {TPQN}")
+
+    ptr+=2
+    # if get_bytes(ptr,ptr+4) == "4d54726b":
+    #     print("\n=== MTrk ===")
+    # else:
+    #     print("x Cannot parse MTrk")
+    #     return ptr, False
+
+    # ptr+=4
+    # mtrk_chk_len = hex2dec(get_bytes(ptr,ptr+4))
+    # print(f"MTrk Chunk Length: {mtrk_chk_len}")
+
+    # ptr+=4
+    return ptr, True
+
+def parse_event(ptr):
+    """ return ptr, (event variable) """
 
     byte = get_bytes(ptr)
 
@@ -69,9 +121,31 @@ def get_event(ptr):
             txt_len = hex2dec(get_bytes(ptr+1))
             ptr+=2
             trk_name = get_bytes(ptr,ptr+txt_len)
-            print("Track name: {trk_name}")
+            print(f"Track name: {trk_name}")
             ptr+=txt_len
-            return ptr, True
+            return ptr, trk_name
+
+        elif byte=="2f":
+            if get_bytes(ptr+1) != "00":
+                print("x Invalid end notation!")
+                return ptr, False
+            print("=== End of track ===")
+            ptr+=2
+            return ptr, "EOT"
+
+        elif byte=="51":
+        # Set Tempo: FF 51 03 tttttt
+        # (ms per MIDI quarter-note)
+            if get_bytes(ptr+1) != "03":
+                print("x Invalid set tempo byte length!")
+                return ptr, False
+            ptr+=2
+            tttttt = hex2dec(get_bytes(ptr,ptr+3))
+            USPQN = tttttt
+            print(f"tttttt: {tttttt} (\u03BCs per MIDI quarter-note)")
+            ptr+=3
+            return ptr, tttttt
+
         elif byte=="58":
         # Time Signature: FF 58 04 nn dd cc bb
         #   nn: numerator
@@ -83,12 +157,14 @@ def get_event(ptr):
                 return ptr, False
             ptr+=2
             nn = hex2dec(get_bytes(ptr))
-            dd = hex2dec(2**get_bytes(ptr+1))
+            dd = 2**hex2dec(get_bytes(ptr+1))
             cc = hex2dec(get_bytes(ptr+2))
             bb = hex2dec(get_bytes(ptr+3))
             print(f"nn:{nn}, dd:{dd}, cc:{cc}, bb:{bb}")
+            NUME, DENO = nn, dd
             ptr+=4
             return ptr,(nn,dd,cc,bb)
+
         elif byte=="59":
         # Key Signature: FF 59 02 sf mi 
         #   sf = -7:  7 flats
@@ -104,47 +180,32 @@ def get_event(ptr):
             ptr+=2
             sf = hex2dec(get_bytes(ptr),signed=True)
             mi = hex2dec(get_bytes(ptr+1),signed=True)
-            print(f"sf:{sf},mi:{mi}")
+            print(f"sf:{sf}, mi:{mi}")
             ptr+=2
             return ptr, (sf,mi)
-        elif byte=="51":
-        # Set Tempo: FF 51 03 tttttt
-        # (ms per MIDI quarter-note)
-            if get_bytes(ptr+1) != "03":
-                print("x Invalid set tempo byte length!")
-                return ptr, False
-            ptr+=2
-            tttttt = hex2dec(get_bytes(ptr,ptr+3))
-            print(f"tttttt: {tttttt}")
-            ptr+=3
-            return ptr, tttttt
-        elif byte=="2f":
-            if get_bytes(ptr+1) != "00":
-                print("x Invalid end notation!")
-                return ptr, False
-            print("=== End of track ===")
-            ptr+=2
-            return ptr, True
+
         else:
             print(f"x Unknown bytes of Meta-Event after {byte}!")
             return ptr, False
+
     elif byte=="4d":
         if get_bytes(ptr+1)=="54":
             ptr+=2
-            if get_bytes(ptr,ptr+2)=="6864":
-                print("=== MThd ===")
+            # if get_bytes(ptr,ptr+2)=="6864":
+            #     print("=== MThd ===")
                 # to add chunk length
                 # return ptr+2
-            elif get_bytes(ptr,ptr+2)=="726b":
+            if get_bytes(ptr,ptr+2)=="726b":
                 print("\n=== MTrk ===")
                 ptr+=2
                 mtrk_chunk_len = hex2dec(get_bytes(ptr,ptr+4))
                 print(f"MTrk chunk length: {mtrk_chunk_len}")
                 ptr+=4
-                return ptr,mtrk_chunk_len
+                return ptr, mtrk_chunk_len
             else:
-                print(f"x Unknown bytes after {byte}!")
+                print(f"x Unknown bytes {get_bytes(ptr)} after 4d54 at ptr {ptr}!")
                 return ptr, False
+
     elif byte[0] in "89":
         # * MIDI Event Table
         # ** http://www33146ue.sakura.ne.jp/staff/iz/formats/midi-event.html
@@ -156,14 +217,16 @@ def get_event(ptr):
             switch = "ON"
         if byte[1] in hexchr:
             chan_num = hexchr.index(byte[1])
-            note_num = hex2dec(get_bytes(ptr+1))
-            note_vel = hex2dec(get_bytes(ptr+2))
-            print(f"Chan {chan_num} note {note_num} {switch}, velocity: {note_vel}"})
+            pit_hex = get_bytes(ptr+1)
+            pit_dec = hex2dec(pit_hex)
+            velocity = hex2dec(get_bytes(ptr+2))
+            print(f"Chan {chan_num} note {pit_dec} {switch:>4}, velocity: {velocity:>3}")
             ptr+=3
-            return ptr, switch
+            return ptr, (switch,chan_num,pit_dec,velocity)
         else:
-            print("x Invalid chan num of note {switch}!")
+            print(f"x Invalid chan num {byte[1]} of note {switch}!")
             return ptr, False
+
     elif byte[0]=="b":
         # * MIDI Control Table 
         # ** http://www33146ue.sakura.ne.jp/staff/iz/formats/midi-cntl.html
@@ -175,59 +238,73 @@ def get_event(ptr):
             #     ptr+=2
             # else:
             #     ctrl_str = "<UNDEFINED>"
-            print(f"Chan {chan_num+1} control mode change to {hex2dec(byte)}")
+            mode_num = hex2dec(byte)
+            print(f"Chan {chan_num} control mode change to 0x{get_bytes(ptr+1,ptr+3)}")
+            # ignore control commands (3 bytes)
             ptr+=3
-            return ptr, True
+            return ptr, (chan_num, mode_num)
         else:
-            print("x Invalid chan num of control!")
+            print(f"x Invalid chan num {byte[1]} of control!")
             return ptr, False
+
     elif byte[0]=="c":
         if byte[1] in hexchr:
             chan_num = hexchr.index(byte[1])
             inst_num = hex2dec(get_bytes(ptr+1))
-            print(f"Channel {chan_num+1} program change to instrument {inst_num}")
+            print(f"Chan {chan_num} program change to instrument {inst_num}")
             ptr+=2
-            return ptr,inst_num
+            return ptr,(chan_num,inst_num)
         else:
             print("x Invalid chan num to change instrument!")
             return ptr, False
     else:
-        print(f"x Unknown bytes after {byte}")
+        print(f"x Unknown bytes {byte} at ptr {ptr}!")
         return ptr, False
 
+active_note_L = []
+played_note_L = []
 
-def parse_midi():
-    if get_bytes(0,4) == "4d546864":
-        print("MThd ...")
-    else:
-        print("x Cannot parse MThd!")
-        return 
+def insert_note(abs_t,res):
+    # played_note: start (tick), dura (tick), chan_num, pit_dec, velocity
+    global active_note_L, played_note_L
+    chan_num, pit_dec, velocity = res[1:4]
+    if res[0] == "ON":
+    # on_note: start (tick), chan_num, pit_dec, velocity
+        active_note_L.append([abs_t,*res[1:4]])
+    elif res[0] == "OFF":
+    # off_note: end (tick), chan_num, pit_dec, velocity
+        for idx,note in enumerate(active_note_L):
+            if pit_dec == note[2]:
+                dura = abs_t - note[0]
+                active_note_L[idx].insert(1,dura)
+                played_note_L.append(active_note_L.pop(idx))
+                break
 
-    MTHD_CHK_LEN = hex2dec(get_bytes(4,8))
-    print(f"MThd Chunk Length: {MTHD_CHK_LEN} (bytes)")
+def process_midi():
+    ptr, res = parse_header()
+    ptr,res = parse_event(ptr)
 
-    TRK_FMT = hex2dec(get_bytes(8,10))
-    print(f"Track Format: {TRK_FMT}")
+    abs_t = 0
+    while res != False:
+        ptr, dt = parse_delta_time(ptr)
+        abs_t += dt
+        ptr, res = parse_event(ptr)
 
-    TRK_NUM = hex2dec(get_bytes(10,12))
-    print(f"Track Number: {TRK_NUM}")
+        if res == "EOT":
+            if get_bytes(ptr) == "":
+                print("\n>>> End of file <<<\n")
+                break
+            else:
+                abs_t = 0
+                ptr,res = parse_event(ptr)
+        elif type(res)==tuple:
+            if res[0] in ["ON","OFF"]:
+                insert_note(abs_t,res)
+        else:
+            pass
 
-    TPQN = hex2dec(get_bytes(12,14))
-    print(f"Ticks Per Quarter-Note: {TPQN}")
-
-    if get_bytes(14,18) == "4d54726b":
-        print("\nMTrk ...")
-    else:
-        print("x Cannot parse MTrk")
-        return 
-
-    MTRK_CHK_LEN = hex2dec(get_bytes(18,22))
-    print(f"MTrk Chunk Length: {MTRK_CHK_LEN}")
-
-    ptr = 22
-    ptr,dt = get_delta_time(ptr)
-
-    print(dt)
 
 if __name__ == '__main__':
-    parse_midi()
+    process_midi()
+    for note in played_note_L:
+        print(note)
