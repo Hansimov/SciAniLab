8456f06c7b8769d7babc6c6d14342393f5998451
Hansimov <591172499@qq.com>
2020-03-31 20:20:09 +0800

    + Add midi note number info

--- technology-presentations/animusic-piano/scripts/midi_parser.py
+++ technology-presentations/animusic-piano/scripts/midi_parser.py
@@ -1,5 +1,9 @@
 # encoding: utf-8
 from __future__ import print_function, division
+import collections
+
+# * Standard MIDI file format, updated 
+# ** http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
 
 # * MIDI文件格式解析 | 码农家园 
 # ** https://www.codenong.com/js59d74800b43b/
@@ -10,16 +14,11 @@ from __future__ import print_function, division
 # * MIDI文件格式分析──理论篇 - Midifan：我们关注电脑音乐 
 # ** https://m.midifan.com/article_body.php?id=901
 
-# * Standard MIDI file format, updated 
-# ** http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
-
 # * Note names, MIDI numbers and frequencies 
 # ** https://newt.phys.unsw.edu.au/jw/notes.html
 
-
-# with open("abc.mid","rb") as rf:
-# with open("secret1.mid","rb") as rf:
-#     bytes_L = ["{:02x}".format(b) for b in rf.read()]
+# * Frequency and Pitch of Sound: From Physclips 
+# ** https://www.animations.physics.unsw.edu.au/jw/frequency-pitch-sound.htm
 
 bytes_L = []
 
@@ -27,7 +26,7 @@ hexchr = "0123456789abcdef"
 
 TPQN = -1   # ticks per quarter-note
 USPQN = -1  # us per quarter-note
-chan_inst_L = [] # channel num <-> instrument num
+chan_inst_L = [] # list of [channel num, instrument num]
 NUME, DENO = -1, -1
 
 
@@ -51,7 +50,7 @@ def hex2bin(hex_str):
     return "{val:0{width}b}".format(val=hex2dec(hex_str),width=bit_num)
 
 
-def parse_delta_time(ptr):
+def read_delta_time(ptr,info_level=0):
     """ return (int, int) : (pointer offset, num of tick) """
     # * Variable Length Values 
     # ** http://www.ccarh.org/courses/253/handout/vlv/
@@ -69,11 +68,12 @@ def parse_delta_time(ptr):
         bin_str+=tmp_str[1:]
     dt = bin2dec(bin_str)
     ptr+=1
-    # print("dt: {} (ticks)".format(dt))
+    if info_level>=3:
+        print("dt: {} (ticks)".format(dt))
     return ptr, dt
 
 
-def parse_event(ptr):
+def read_event(ptr,info_level=1):
     """ return ptr, (event vars) """
     global USPQN, NUME, DENO
     byte = get_bytes(ptr)
@@ -89,7 +89,8 @@ def parse_event(ptr):
             txt_len = hex2dec(get_bytes(ptr+1))
             ptr+=2
             trk_name = get_bytes(ptr,ptr+txt_len)
-            print("Track name: {}".format(trk_name))
+            if info_level>=2:
+                print("Track name: {}".format(trk_name))
             ptr+=txt_len
             return ptr, trk_name
 
@@ -97,7 +98,8 @@ def parse_event(ptr):
             if get_bytes(ptr+1) != "00":
                 print("x Invalid end notation!")
                 return ptr, False
-            print("=== End of track ===")
+            if info_level>=2:
+                print("=== End of track ===")
             ptr+=2
             return ptr, "EOT"
 
@@ -109,7 +111,8 @@ def parse_event(ptr):
                 return ptr, False
             ptr+=2
             USPQN = hex2dec(get_bytes(ptr,ptr+3))
-            print("USPQN: {} (\u03BCs per quarter-note)".format(USPQN))
+            if info_level>=2:
+                print("USPQN: {} (us per quarter-note)".format(USPQN))
             ptr+=3
             return ptr, USPQN
 
@@ -127,7 +130,8 @@ def parse_event(ptr):
             dd = 2**hex2dec(get_bytes(ptr+1))
             cc = hex2dec(get_bytes(ptr+2))
             bb = hex2dec(get_bytes(ptr+3))
-            print("nn:{}, dd:{}, cc:{}, bb:{}".format(nn,dd,cc,bb))
+            if info_level>=2:
+                print("nn:{}, dd:{}, cc:{}, bb:{}".format(nn,dd,cc,bb))
             ptr+=4
             NUME,DENO = nn, dd
             return ptr,(nn,dd,cc,bb)
@@ -147,7 +151,8 @@ def parse_event(ptr):
             ptr+=2
             sf = hex2dec(get_bytes(ptr),signed=True)
             mi = hex2dec(get_bytes(ptr+1),signed=True)
-            print("sf:{}, mi:{}".format(sf,mi))
+            if info_level>=2:
+                print("sf:{}, mi:{}".format(sf,mi))
             ptr+=2
             return ptr, (sf,mi)
 
@@ -159,10 +164,12 @@ def parse_event(ptr):
         if get_bytes(ptr+1)=="54":
             ptr+=2
             if get_bytes(ptr,ptr+2)=="726b":
-                print("\n=== MTrk ===")
+                if info_level>=2:
+                    print("\n=== MTrk ===")
                 ptr+=2
                 mtrk_chk_len = hex2dec(get_bytes(ptr,ptr+4))
-                print("MTrk chunk length: {} (bytes)".format(mtrk_chk_len))
+                if info_level>=2:
+                    print("MTrk chunk length: {} (bytes)".format(mtrk_chk_len))
                 ptr+=4
                 return ptr, mtrk_chk_len
             else:
@@ -183,7 +190,8 @@ def parse_event(ptr):
             pit_hex = get_bytes(ptr+1)
             pit_dec = hex2dec(pit_hex)
             velocity = hex2dec(get_bytes(ptr+2))
-            # print("Chan {} note {} {:>3}, velocity: {:>2}".format(chan_num,pit_dec,switch,velocity))
+            if info_level>=3:
+                print("Chan {} note {} {:>3}, velocity: {:>2}".format(chan_num,pit_dec,switch,velocity))
             ptr+=3
             return ptr, (switch,chan_num,pit_dec,velocity)
         else:
@@ -209,7 +217,8 @@ def parse_event(ptr):
         if byte[1] in hexchr:
             chan_num = hexchr.index(byte[1])
             inst_num = hex2dec(get_bytes(ptr+1))
-            print("Chan {} program change to instrument {}".format(chan_num,inst_num))
+            if info_level>=2:
+                print("Chan {} program change to instrument {}".format(chan_num,inst_num))
             ptr+=2
             chan_inst_L.append([chan_num,inst_num])
             return ptr,(chan_num,inst_num)
@@ -239,45 +248,53 @@ def insert_note(abs_t,res):
                 played_note_L.append(active_note_L.pop(idx))
                 break
 
-def parse_mthd():
+def read_mthd(info_level=2):
     global TPQN
     ptr = 0
     if get_bytes(ptr,ptr+4) == "4d546864":
-        print("=== MThd ===")
+        if info_level>=2:
+            print("=== MThd ===")
     else:
         print("x Cannot parse MThd!")
         return ptr, False
 
     ptr+=4
     mthd_chk_len = hex2dec(get_bytes(ptr,ptr+4))
-    print("MThd chunk length: {} (bytes)".format(mthd_chk_len))
+    if info_level>=2:
+        print("MThd chunk length: {} (bytes)".format(mthd_chk_len))
 
     ptr+=4
     trk_fmt = hex2dec(get_bytes(ptr,ptr+2))
-    print("Track Format: {}".format(trk_fmt))
+    if info_level>=2:
+        print("Track Format: {}".format(trk_fmt))
 
     ptr+=2
     trk_num = hex2dec(get_bytes(ptr,ptr+2))
-    print("Track Number: {}".format(trk_num))
+    if info_level>=2:
+        print("Track Number: {}".format(trk_num))
 
     ptr+=2
     TPQN = hex2dec(get_bytes(ptr,ptr+2))
-    print("TPQN: {} (ticks per quarter-note)".format(TPQN))
+    if info_level>=2:
+        print("TPQN: {} (ticks per quarter-note)".format(TPQN))
 
     ptr+=2
     return ptr, True
 
-def parse_mtrk(ptr):
-    ptr, res = parse_event(ptr)
+def read_mtrk(ptr,info_level):
+    ptr, res = read_event(ptr,info_level)
     abs_t = 0
     while res != False:
-        ptr, dt = parse_delta_time(ptr)
+        ptr, dt = read_delta_time(ptr,info_level)
         abs_t += dt
-        ptr, res = parse_event(ptr)
+        ptr, res = read_event(ptr,info_level)
 
         if res == "EOT":
             if get_bytes(ptr) == "":
-                print("\n>>> End of file <<<\n")
+                if info_level>=2:
+                    print("\n>>> End of file <<<\n")
+                if info_level>=1:
+                    print("MIDI file processed successfully!")
                 return ptr, "EOF"
             else:
                 return ptr, "EOT"
@@ -287,22 +304,49 @@ def parse_mtrk(ptr):
         else:
             pass
 
-def process_midi(filename):
+def process_midi(filename,info_level=1):
+    # info_level: 0: None | 1: warnig | 2: info | 3: details
     global bytes_L, played_note_L
     # with open("abc.mid","rb") as rf:
     with open(filename,"rb") as rf:
         # Note: py2:ord(b) || py3:b
         bytes_L = ["{:02x}".format(ord(b)) for b in rf.read()]
 
-    ptr, res = parse_mthd()
+    ptr, res = read_mthd(info_level)
     while res != "EOF":
-        ptr,res = parse_mtrk(ptr)
+        ptr,res = read_mtrk(ptr,info_level)
     played_note_L = sorted(played_note_L,key=lambda l:l[0])
     # for played_note in played_note_L:
     #     print(played_note)
-    print(TPQN, USPQN, NUME, DENO, chan_inst_L)
+    # print(TPQN, USPQN, NUME, DENO, chan_inst_L)
+    return played_note_L
+
+
+# Table of MIDI Note Numbers
+
+# ---|-------------------------------------------------
+# Oct|                  Note Numbers
+# ---|-------------------------------------------------
+#    |   C  C#   D  D#   E   F  F#   G  G#   A  A#   B 
+# ---|-------------------------------------------------
+# -1 |   0   1   2   3   4   5   6   7   8   9  10  11 
+#  0 |  12  13  14  15  16  17  18  19  20  21  22  23 
+#  1 |  24  25  26  27  28  29  30  31  32  33  34  35 
+#  2 |  36  37  38  39  40  41  42  43  44  45  46  47 
+#  3 |  48  49  50  51  52  53  54  55  56  57  58  59 
+#  4 |  60  61  62  63  64  65  66  67  68  69  70  71 
+#  5 |  72  73  74  75  76  77  78  79  80  81  82  83 
+#  6 |  84  85  86  87  88  89  90  91  92  93  94  95 
+#  7 |  96  97  98  99 100 101 102 103 104 105 106 107 
+#  8 | 108 109 110 111 112 113 114 115 116 117 118 119 
+#  9 | 120 121 122 123 124 125 126 127                
+# ---|-------------------------------------------------
+
+# Middle C = C4 (60,0x3c)
+# C4 is 40th key on 88-key piano keyboards
+# 88-key range: A0-C8 | 21-108 | 0x15-0x80
 
 if __name__ == '__main__':
     # process_midi("abc.mid")
-    process_midi("secret1.mid")
+    process_midi("secret1.mid",info_level=1)
 
--- technology-presentations/animusic-piano/scripts/piano_fingering.py
+++ technology-presentations/animusic-piano/scripts/piano_fingering.py
@@ -3,6 +3,9 @@
 # * Correct Piano Fingering: The Why and How 
 # ** https://www.onlinepianocoach.com/piano-fingering.html
 
+# * Finger positions – four piano finger exercises that will set you on your way 
+# ** https://www.skoove.com/blog/finger-positions-four-piano-finger-exercises-that-will-set-you-on-your-way/
+
 # * 简单分享：钢琴入门指法的知识 - 简书 
 # ** https://www.jianshu.com/p/d15ddddb745e
 
@@ -24,6 +27,50 @@ from midi_parser import *
 from c4d_kits import *
 
 clear_console()
-# process_midi(path_to_add+"abc.mid")
-process_midi(path_to_add+"secret1.mid")
+process_midi(path_to_add+"abc.mid",info_level=1)
+# process_midi(path_to_add+"secret1.mid",info_level=1)
+# note: start, dura, chan_num, pit_dec, vel
+# for note in played_note_L:
+#     print(note)
+# print(len(played_note_L))
 
+pit_L = [note[3] for note in played_note_L]
+# print(len(pit_L))
+# for pit in pit_L:
+#     print(pit)
+
+# a = [1,1,1,1,2,2,2,2,3,3,4,5,5]
+# counter = collections.Counter(pit_L)
+
+# for key,val in zip(counter.keys(),counter.values()):
+#     print(key,val)
+# for i in counter.most_common():
+#     print(i)
+
+
+# chords = find_obj("chords","")[0]
+# for i,chord in enumerate(chords.GetChildren()):
+#     print(chord.GetName())
+#     chord.SetName("chord_{:03}".format(i+21))
+    # print(chord.GetName())
+
+# for key in keys:
+#     print(key.GetName())
+
+chords = find_obj("chord_.*","chords")
+
+arms_l = find_obj("arm_L.*","arms")
+arms_r = find_obj("arm_R.*","arms")
+
+# def init_arms_position():
+#     pass
+    # print(get_rel_pos(find_obj))
+    # print(get_rel_pos(arms_l[0]))
+
+# anchor_chord = [60,64,67,]
+# init_arms_position()
+
+for chord in chords:
+    
+
+chords[0][c4d.ID_BASEOBJECT_COLOR] = c4d.Vector(1,0,0)
