34900ddb24b2f18ad3ea323b0f3fa35b9559ea27
Hansimov <591172499@qq.com>
2020-04-25 11:14:57 +0800

    + Add multi-threading test_proxy

--- data-visualizations/bili-comment/test_proxy.py
+++ data-visualizations/bili-comment/test_proxy.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import subprocess
 import requests
 import socket
@@ -9,48 +10,28 @@ import re
 import threading
 
 # ip, port, kind = "117.88.176.162", "3000", "https"
-ip, port, kind = "124.156.98.172", "80", "http"
+# ip, port, kind = "124.156.98.172", "80", "http"
 
 url = "{}://api.bilibili.com/x/v2/reply?pn=1&type=1&oid=667592495&sort=2&_=1587689751023"
 
 http_D = {
-    "http":  "http://{}:{}".format(ip,port),
-    "https": "https://{}:{}".format(ip,port),
-    "ftp":   "ftp://{}:{}".format(ip,port)
+    "http":  "http://{}:{}",
+    "https": "https://{}:{}",
+    "ftp":   "ftp://{}:{}"
 }
 
 headers = {
     "user-agent": "skynet - {}".format(random.random())
 }
 
-def test_ip_port(ip,port,retry=3,timeout=3):
-    semalock = 
-    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    skt.settimeout(timeout)
-    ex_code = skt.connect_ex((ip, int(port)))
-    # a = s.connect((ip, int(port)))
-    # s.shutdown(socket.SHUT_RDWR)
-    # print(ex_code)
-    skt.close()
 
-    ret = 0
-    if ex_code == 0:
-        # print("+++ {}:{} open +++".format(ip,port))
-        print("+++ OPEN +++".format(ip,port))
-        ret = 1
-    else:
-        # print("--- {}:{} fail ---".format(ip,port))
-        print("------------".format(ip,port))
-    lock.acquire()
-    sys.stdout.flush()
-    lock.release()
-
-    return ret
+def is_any_thread_alive(threads):
+    return True in [t.is_alive() for t in threads]
 
 
 def fetch_proxy_site():
     req = requests.get("https://www.xicidaili.com/nn/",headers=headers)
-    print("=== Fetching xici successfully ===")
+    print("=== Fetching xici successfully ===\n")
     sys.stdout.flush()
 
     # text = req.text
@@ -75,9 +56,34 @@ def fetch_proxy_site():
     return ip_L
 
 
+def test_ip_port(ip,port,retry=3,timeout=3):
+    global valid_ip_cnt
+    sema.acquire()
+    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    skt.settimeout(timeout)
+    ex_code = skt.connect_ex((ip, int(port)))
+    # a = s.connect((ip, int(port)))
+    # s.shutdown(socket.SHUT_RDWR)
+    # print(ex_code)
+    skt.close()
+
+    ret = 0
+    if ex_code == 0:
+        # print("+++ Valid {:>3}.{:>3}.{:>3}.{:>3} : {:<5} +++++++".format(*ip.split("."),port))
+        print("+++ Valid {:<15} {:<5} +++++++".format(ip,port))
+        ret = 1
+    else:
+        # print("--------- {:>3}.{:>3}.{:>3}.{:>3} : {:<5} -------".format(*ip.split("."),port))
+        print("--------- {:<15} {:<5} -------".format(ip,port))
+    lock.acquire()
+    sys.stdout.flush()
+    valid_ip_cnt += ret
+    lock.release()
+    sema.release()
+
 def request_with_proxy(url_body,ip,port,kind):
     cur_url = url_body.format(kind)
-    cur_proxy = {kind: http_D[kind]}
+    cur_proxy = {kind: http_D[kind].format(ip,port)}
     print(http_D[kind])
 
     try:
@@ -88,18 +94,27 @@ def request_with_proxy(url_body,ip,port,kind):
         # print("- Connection failed!")
         print(e)
 
-# request_with_proxy(url,ip,port,kind)
-
-# test_ip_port(ip,port)
-
-ip_L = fetch_proxy_site()
 
 if __name__ == '__main__':
-    main()
-# open_cnt = 0
-# total_cnt = len(ip_L)
-# # total_cnt = 4
-# for ip in ip_L[:total_cnt]:
-#     print("{:<15} {:<5} {:<5} {:>6} {:<14}".format(ip[0],ip[1],ip[2],ip[3],ip[4]))
-#     open_cnt += test_ip_port(ip[0],ip[1])
-# print("Good IP: {}/{}".format(open_cnt,total_cnt))
+    ip_L = fetch_proxy_site()
+    # request_with_proxy(url,ip,port,kind)
+    # test_ip_port(ip,port)
+    max_concurrent_num = 40
+    sema = threading.BoundedSemaphore(max_concurrent_num)
+    lock = threading.Lock()
+
+    total_ip_cnt = len(ip_L)
+    valid_ip_cnt = 0
+
+    pool = []
+    for i in range(total_ip_cnt):
+        ip,port = ip_L[i][0:2]
+        tmp_thread = threading.Thread(target=test_ip_port, args=(ip,port,), daemon=True)
+        pool.append(tmp_thread)
+    for tmp_thread in pool:
+        tmp_thread.start()
+
+    while is_any_thread_alive(pool):
+        time.sleep(0)
+
+    print("Valid IP: {}/{}".format(valid_ip_cnt, total_ip_cnt))
