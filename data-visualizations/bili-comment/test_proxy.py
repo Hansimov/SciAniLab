22cfe58f81b0d387968e52463ca11682a10461ee
Hansimov <591172499@qq.com>
2020-04-25 16:39:21 +0800

    + Add free-proxy-list fetch module

--- data-visualizations/bili-comment/test_proxy.py
+++ data-visualizations/bili-comment/test_proxy.py
@@ -12,9 +12,9 @@ import threading
 # ip, port, kind = "117.88.176.162", "3000", "https"
 # ip, port, kind = "124.156.98.172", "80", "http"
 
-url = "{}://api.bilibili.com/x/v2/reply?pn=1&type=1&oid=667592495&sort=2&_=1587689751023"
+url_body = "{}://api.bilibili.com/x/v2/reply?pn=1&type=1&oid=34354599&nohot=1&sort=2"
 
-http_D = {
+kind_D = {
     "http":  "http://{}:{}",
     "https": "https://{}:{}",
     "ftp":   "ftp://{}:{}"
@@ -29,9 +29,12 @@ def is_any_thread_alive(threads):
     return True in [t.is_alive() for t in threads]
 
 
-def fetch_proxy_site():
-    req = requests.get("https://www.xicidaili.com/nn/",headers=headers)
-    print("=== Fetching xici successfully ===\n")
+def fetch_xici():
+    # tmp_proxy = {"https":"https://117.45.139.84:9006"}
+    # tmp_proxy = {"http":"http://52.69.132.45:8080"}
+    # req = requests.get("http://www.xicidaili.com/nn/1",headers=headers,proxies=tmp_proxy)
+    req = requests.get("http://www.xicidaili.com/nn/1",headers=headers)
+    print("=== Fetching xici {} ===\n".format(req.status_code))
     sys.stdout.flush()
 
     # text = req.text
@@ -41,75 +44,128 @@ def fetch_proxy_site():
         text = rf.read()
         
     text = re.findall(r"<table[\s\S]*?table>", text)[0]
-    res_L = re.findall(r"<tr class[\s\S]*?tr>", text)
+    tr_L = re.findall(r"<tr class[\s\S]*?tr>", text)
 
     ip_L = []
-    for res in res_L:
-        item = re.findall(r"<td>([\s\S]*?)</td>",res)
-        ip = []
-        for it in item:
-            if "href" in it or "\n" in it:
-                continue
-            ip.append(it)
+    for tr in tr_L:
+        td_L = re.findall(r"<td>([\s\S]*?)</td>",tr)
+        ip = [td_L[0],td_L[1],td_L[-1]]
+        # for td in td_L:
+        #     if "href" in td or "\n" in td:
+        #         continue
+        #     ip.append(td)
         ip_L.append(ip)
-    # ip, port, http(s), alive_time, last_scan_time
+    # ip, port, http(s)
     return ip_L
 
+def fetch_free_proxy():
+    # url = "https://free-proxy-list.net/"
+    # req = requests.get(url,headers=headers)
+    # print("=== Fetching free-proxy-list {} ===\n".format(req.status_code))
+    # # print(req.text)
+    # with open("free-proxy-list.html","wb") as wf:
+    #     wf.write(req.content)
+    with open("free-proxy-list.html",mode="r",encoding="utf-8") as rf:
+        text = rf.read()
 
-def test_ip_port(ip,port,retry=3,timeout=3):
-    global valid_ip_cnt
+    text = re.findall(r"<tbody[\s\S]*?tbody>", text)[0]
+    tr_L = re.findall(r"<tr[\s\S]*?tr>", text)
+
+    ip_L = []
+    for tr in tr_L:
+        td_L = re.findall(r"<td[\s\S]*?>([\s\S]*?)</td>",tr)
+        kind = "http" if td_L[-2]== "no" else "https"
+        ip = [td_L[0],td_L[1],kind]
+        ip_L.append(ip)
+        # print(ip)
+    # ip, port, http(s)
+    return ip_L
+
+valid_ip_L = []
+def test_ip_port(ip,port,kind="http",retry_max=3,timeout=4):
+    global valid_ip_cnt, valid_ip_L
     sema.acquire()
-    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    skt.settimeout(timeout)
-    ex_code = skt.connect_ex((ip, int(port)))
-    # a = s.connect((ip, int(port)))
-    # s.shutdown(socket.SHUT_RDWR)
-    # print(ex_code)
-    skt.close()
+    
+    hit_cnt = 0
+    retry_cnt = 0
+    while (retry_cnt<retry_max):
+        skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        skt.settimeout(timeout)
+        ex_code = skt.connect_ex((ip, int(port)))
+        skt.close()
+        if ex_code == 0:
+            hit_cnt += 1
+        retry_cnt += 1
 
+    # if hit_cnt > 0:
+    #     # print("+++ Valid {:>3}.{:>3}.{:>3}.{:>3} : {:<5} +++++++".format(*ip.split("."),port))
+    #     print("{:<3} {:<15} {:<5} {:<5} {:>3}".format(hit_cnt*"+",ip,port,kind,hit_cnt*"+"))
+    # else:
+    #     # print("--------- {:>3}.{:>3}.{:>3}.{:>3} : {:<5} -------".format(*ip.split("."),port))
+    #     print("{:<3} {:<15} {:<5} {:<5} {:>3}".format(3*"-",ip,port,kind,3*"-"))
+
+    req_cnt = 10
     ret = 0
-    if ex_code == 0:
-        # print("+++ Valid {:>3}.{:>3}.{:>3}.{:>3} : {:<5} +++++++".format(*ip.split("."),port))
-        print("+++ Valid {:<15} {:<5} +++++++".format(ip,port))
+    if hit_cnt > retry_max/2:
         ret = 1
-    else:
-        # print("--------- {:>3}.{:>3}.{:>3}.{:>3} : {:<5} -------".format(*ip.split("."),port))
-        print("--------- {:<15} {:<5} -------".format(ip,port))
+        req_cnt = request_with_proxy(url_body,ip,port,kind)
+        # print(req_cnt)
+
+    # req_cnt = request_with_proxy(url_body,ip,port,kind)
+    # ret = 0
     lock.acquire()
-    sys.stdout.flush()
-    valid_ip_cnt += ret
+    # sys.stdout.flush()
+    if req_cnt <= 1:
+        ret = 1
+        valid_ip_L.append([ip,port,kind])
+        valid_ip_cnt += ret
+        print("{:<3} {:<15} {:<5} {:<5}".format((3-req_cnt)*"+",ip,port,kind.upper()))
+    else:
+        print("{:<3} {:<15} {:<5} {:<5}".format(3*"-",ip,port,kind.upper()))
+
     lock.release()
     sema.release()
 
-def request_with_proxy(url_body,ip,port,kind):
+def request_with_proxy(url_body,ip,port,kind,retry_max=3,timeout=6):
+    kind = kind.lower()
     cur_url = url_body.format(kind)
-    cur_proxy = {kind: http_D[kind].format(ip,port)}
-    print(http_D[kind])
+    cur_proxy = {kind: kind_D[kind].format(ip,port)}
 
-    try:
-        r = requests.get(cur_url, headers=headers, proxies=cur_proxy,timeout=5)
-        print(r)
-        # print(r.json())
-    except Exception as e:
-        # print("- Connection failed!")
-        print(e)
+    retry_cnt = 0
+    while (retry_cnt<retry_max):
+        try:
+            r = requests.get(cur_url, headers=headers, proxies=cur_proxy,timeout=timeout)
+            # print(r.status_code)
+            status_code = r.status_code
+            # print(r.json())
+            # return status_code
+            # return retry_cnt
+            break
+        except Exception as e:
+            # print("- Connection failed!")
+            # print(e)
+            # print("Retry cnt: {}".format(retry_cnt))
+            retry_cnt += 1
+    return retry_cnt
 
+valid_ip_cnt = 0
 
-if __name__ == '__main__':
-    ip_L = fetch_proxy_site()
+max_concurrent_num = 50
+sema = threading.BoundedSemaphore(max_concurrent_num)
+lock = threading.Lock()
+
+def get_valid_ip_list():
+    # ip_L = fetch_xici()
+    ip_L = fetch_free_proxy()
     # request_with_proxy(url,ip,port,kind)
     # test_ip_port(ip,port)
-    max_concurrent_num = 40
-    sema = threading.BoundedSemaphore(max_concurrent_num)
-    lock = threading.Lock()
 
     total_ip_cnt = len(ip_L)
-    valid_ip_cnt = 0
 
     pool = []
     for i in range(total_ip_cnt):
-        ip,port = ip_L[i][0:2]
-        tmp_thread = threading.Thread(target=test_ip_port, args=(ip,port,), daemon=True)
+        ip,port,kind = ip_L[i][0:3]
+        tmp_thread = threading.Thread(target=test_ip_port, args=(ip,port,kind,), daemon=True)
         pool.append(tmp_thread)
     for tmp_thread in pool:
         tmp_thread.start()
@@ -117,4 +173,18 @@ if __name__ == '__main__':
     while is_any_thread_alive(pool):
         time.sleep(0)
 
-    print("Valid IP: {}/{}".format(valid_ip_cnt, total_ip_cnt))
+    print("\nValid IP: {}/{}".format(valid_ip_cnt, total_ip_cnt))
+    for valid_ip in valid_ip_L:
+        print("   {:<15} {:<5} {:<5}".format(*valid_ip[:3]))
+
+if __name__ == '__main__':
+
+    t1 = time.time()
+    get_valid_ip_list()
+    t2 = time.time()
+    print("Elapsed time: {} sec".format(round(t2-t1,2)))
+
+    # fetch_free_proxy()
+    # get_valid_ip_list()
+
+
