f728c8ed08f7b0dda36eeec93a376528b8a2fd52
Hansimov <591172499@qq.com>
2020-05-01 23:47:58 +0800

    + Update proxy_server

--- data-visualizations/bili-comment/proxy_server.py
+++ data-visualizations/bili-comment/proxy_server.py
@@ -1,21 +1,212 @@
 import os
 import sys
 import time
+import datetime
+import random
+import re
+import requests
 import socket
+import threading
 import pickle
 
 # fetch proxies in advance
 # if a proxy in client is invalid, then the client requests for a new proxy
 # server returns a valid proxy when receiving a client request
 
-def select_proxy(conn):
-    proxy = ["123.123.123.123","12345","https"]
-    proxy_pkl = pickle.dumps(proxy)
-    conn.sendall(proxy_pkl)
-    print("Sending {:<15} {:<5} {:<5}".format(*proxy[:3]))
+last_fetch_proxy_time = 0.0
+
+valid_proxy_L, invalid_proxy_L = [], []
+
+def headers():
+    return { "user-agent": "botnet - {}".format(random.random()) }
+
+def dt2str(dt_time):
+    return "{:>4}-{:0>2}-{:0>2}-{:0>2}-{:0>2}-{:0>2}".format(dt_time.year, dt_time.month, dt_time.day, dt_time.hour, dt_time.minute, dt_time.second)
+
+def str2dt(dt_str):
+    return datetime.datetime.strptime(dt_str,'%Y-%m-%d-%H-%M-%S')
+
+def is_any_thread_alive(threads):
+    return True in [t.is_alive() for t in threads]
+
+def is_proxy_in_proxy_L(proxy, proxy_L, valid_proxy_L_lock):
+    valid_proxy_L_lock.acquire()
+
+    for proxy_tmp in proxy_L:
+        if proxy[:3] == proxy_tmp[:3]:
+            # print(proxy,proxy_tmp)
+            valid_proxy_L_lock.release()
+            return True
+
+    valid_proxy_L_lock.release()
+    return False
+
+def ip_port_to_proxy(ip,port,kind):
+    kind_D = {
+        "http":  "http://{}:{}",
+        "https": "https://{}:{}",
+        "ftp":   "ftp://{}:{}"
+    }
+    return {kind: kind_D[kind].format(ip,port)}
 
 
-def run_server(timeout=2):
+def fetch_free_proxy():
+    """ return fetched_proxy_L 
+        2d list of [ip, port, kind, last_used_time]
+    """
+    global last_fetch_proxy_time
+    old_filename = ""
+    for filename in os.listdir():
+        if re.match("free-proxy-list-[\s\S]*",filename):
+            old_filename = filename
+            break
+
+    new_dt_time = datetime.datetime.now()
+    last_fetch_proxy_time = time.time()
+
+    is_fetch_new_proxy = False
+
+    if old_filename == "":
+        is_fetch_new_proxy = True
+    else:
+        old_dt_str = old_filename.replace("free-proxy-list-","").replace(".html","")
+        old_dt_time = str2dt(old_dt_str)
+        if (new_dt_time-old_dt_time).total_seconds() > 300:
+            os.remove(old_filename)
+            is_fetch_new_proxy = True
+
+    if is_fetch_new_proxy:
+        url = "https://free-proxy-list.net/"
+        req = requests.get(url,headers=headers())
+        print("=== Fetching free-proxy-list {} ===\n".format(req.status_code))
+        new_filename = "free-proxy-list-{}.html".format(dt2str(new_dt_time))
+        with open(new_filename,"wb") as wf:
+            wf.write(req.content)
+        read_filename = new_filename
+    else:
+        print("=== Reusing {}\n".format(old_filename))
+        read_filename = old_filename
+
+    with open(read_filename,mode="r",encoding="utf-8") as rf:
+        text = rf.read()
+
+    text = re.findall(r"<tbody[\s\S]*?tbody>", text)[0]
+    tr_L = re.findall(r"<tr[\s\S]*?tr>", text)
+
+    fetched_proxy_L = []
+    for tr in tr_L:
+        td_L = re.findall(r"<td[\s\S]*?>([\s\S]*?)</td>",tr)
+        kind = "http" if td_L[-2]== "no" else "https"
+        proxy = [td_L[0], td_L[1], kind]
+        fetched_proxy_L.append(proxy)
+    # ip, port, http(s)
+    return fetched_proxy_L
+
+
+req_retry_max = 3
+req_timeout = 3.0
+# reply_url_body = "{}://api.bilibili.com/x/v2/reply?pn={}&type=1&oid={}&nohot=1&sort=2"
+def request_with_proxy(url_body,ip,port,kind,retry_max=req_retry_max,timeout=req_timeout):
+    kind = kind.lower()
+    cur_url = url_body.format(kind)
+    cur_proxy = ip_port_to_proxy(ip,port,kind)
+
+    retry_cnt = 0
+    # t1 = time.time()
+    while (retry_cnt<req_retry_max):
+        try:
+            with eventlet.Timeout(timeout):
+                r = requests.get(cur_url, headers=headers(), proxies=cur_proxy)
+                status_code = r.status_code
+            break
+        except:
+            # print(e)
+            retry_cnt += 1
+    return retry_cnt
+
+def check_proxy_validity(proxy, check_proxy_connection_sema, valid_proxy_L_lock):
+    global valid_proxy_L
+
+    check_proxy_connection_sema.acquire()
+    ip, port, kind = proxy[:3]
+
+    is_proxy_valid = False
+    if is_proxy_in_proxy_L(proxy, invalid_proxy_L, valid_proxy_L_lock):
+        pass
+    else:
+        test_url_body = "{}://api.bilibili.com/x/v2/reply?pn=1&type=1&oid=34354599&nohot=1&sort=2"
+        t1 = time.time()
+        req_cnt = request_with_proxy(test_url_body,ip,port,kind)
+        t2 = time.time()
+
+        # sys.stdout.flush()
+        if req_cnt <= 1:
+            # ip, port, kind, last_used_time, delay
+            is_proxy_valid = True
+
+    valid_proxy_L_lock.acquire()
+    if is_proxy_valid:
+        delta_t = round(t2-t1,1)
+        valid_proxy_L.append([ip, port, kind, time.time(), delta_t])
+    else:
+        delta_t = req_retry_max*req_timeout
+        req_cnt = 3
+        invalid_proxy_L.append([ip, port, kind, time.time(), delta_t])
+    valid_proxy_L_lock.release()
+
+    print("{:<3} {:<15} {:<5} {:<5} {:>4}s".format((3-req_cnt)*"+", ip, port, kind.upper(), delta_t))
+
+    check_proxy_connection_sema.release()
+
+update_valid_proxy_interval = 300
+def update_valid_proxy():
+    """ update valid_proxy_L and invalid_proxy_L: 
+        2d list of [ip, port, kind, last_used_time, delay]
+    """
+    global valid_proxy_L, invalid_proxy_L
+
+    if time.time() - last_fetch_proxy_time < update_valid_proxy_interval:
+        return
+    # fetched_proxy_L = fetch_xici()
+    fetched_proxy_L = fetch_free_proxy()
+    check_proxy_validity_sema = threading.BoundedSemaphore(len(fetched_proxy_L))
+
+    valid_proxy_L_lock = threading.Lock()
+
+    pool = []
+    for i in range(len(fetched_proxy_L)):
+        # proxy = fetched_proxy_L[i][:3]
+        # ip,port,kind
+        proxy = fetched_proxy_L[i]
+        tmp_thread = threading.Thread(target=check_proxy_validity, args=(proxy,  check_proxy_validity_sema, valid_proxy_L_lock), daemon=True)
+        pool.append(tmp_thread)
+
+    for tmp_thread in pool:
+        tmp_thread.start()
+    # for tmp_thread in pool:
+    #     tmp_thread.join()
+
+    while is_any_thread_alive(pool):
+        time.sleep(0)
+
+reuse_interval = 1.0
+def select_valid_proxy(conn):
+    global valid_proxy_L
+    if len(valid_proxy_L) > 0:
+        proxy = valid_proxy_L[0]
+        if time.time() - proxy[3] > reuse_interval:
+            proxy_pkl = pickle.dumps(proxy)
+            conn.sendall(proxy_pkl)
+            print("Sending {:<15} {:<5} {:<5}".format(*proxy[:3]))
+            proxy[3] = time.time()
+            valid_proxy_L.append(proxy)
+            valid_proxy_L.pop(0)
+            return True
+    return False
+
+
+socket_timeout = 1
+def run_server(timeout=socket_timeout):
     host = "127.0.0.1"
     port = 23333
 
@@ -37,15 +228,15 @@ def run_server(timeout=2):
                     # print("> Message from client: {}".format(data.decode()))
                     # msg = "> Message from server".format(data.decode()).encode()
                     # conn.sendall(msg)
-                    select_proxy(conn)
+                    select_valid_proxy(conn)
             except socket.timeout:
-                print("Timeout")
+                # print("Timeout")
+                update_valid_proxy()
             except KeyboardInterrupt:
                 pass
     except KeyboardInterrupt:
         print("Server closed with KeyboardInterrupt!")
 
-
 if __name__ == '__main__':
-    run_server(2)
+    run_server()
 
